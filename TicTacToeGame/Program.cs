1b8a8569e0cbd11f471d3ba2e9d23cf0f51f4d40
Priya Biriya <priyabiriya1707@gmail.com>
2020-10-06 00:37:56 +0530

    [Priya] add . choosing letter

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -8,6 +8,8 @@ namespace TICTacToeGame
             Console.WriteLine("Welcome to Tic Tac Toe!");
             var ticTacToeGame = new TicTacToeGame();
             ticTacToeGame.InitializeBoard();
+            ticTacToeGame.player = ticTacToeGame.ChooseOption();
+            ticTacToeGame.computer = ticTacToeGame.player == 'X' ? 'O' : 'X';
         }
     }
 }
--- TicTacToeGame/TicTacToeGame.cs
+++ TicTacToeGame/TicTacToeGame.cs
@@ -7,11 +7,19 @@ namespace TICTacToeGame
     class TicTacToeGame
     {
         public char[] board { get; set; }
+        public char player { get; set; }
+        public char computer { get; set; }
         public void InitializeBoard()
         {
             board = new char[10];
             for (int i = 1; i < 10; i++)
                 board[i] = ' ';
+        }
+        public char ChooseOption()
+        {
+            Console.Write("Choose X or O : ");
+            var input = Convert.ToChar(Console.ReadLine());
+            return (input == 'X' || input == 'O') ? input : ChooseOption();
         }
     }
 }
