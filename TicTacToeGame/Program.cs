89bebc7f8ccf71f61797831fe34cae7d704d452c
Priya Biriya <priyabiriya1707@gmail.com>
2020-10-06 00:01:16 +0530

    [Priya] add . sentence

---
+++ TicTacToeGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tic Tac Toe player would like to challange computer!");
+        }
+    }
+}
